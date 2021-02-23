1cc472c17c89500d1367ab439d24e92aa9544234
rrxrs <kadoshrr2019@gmail.com>
2021-02-23 13:30:52 -0500

    Added validators

--- log.js
+++ log.js
@@ -11,3 +11,9 @@
 //* sequelize init
 //* configuración de directorios y archivos sequelize
 //* modelos y migración de postgres
+
+//+ 20210220
+
+//*Directorios de controllers, middlewares y routes
+//*Archivos auth.js (login & signup) y users.js (getUsers)
+//*Archivo jwt.js (generateJWT)
